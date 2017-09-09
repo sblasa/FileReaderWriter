e9a2e7a1c3028e4487b1b6c3614d2845de064730
sblasa <stblasa@gmail.com>
2017-09-09 16:37:48 -0700

    added sblasaFileReader.js file

--- sblasaFileReader.js
+++ sblasaFileReader.js
@@ -1,41 +1,11 @@
-var fs = require("fs"), 
+var fs = require("fs"),
     fileName = "fsdata/data.txt";
 
-
-console.log("Starting to load.");
-
-fs.stat("fsdata", function (err,stat) {
-    console.log("Check status of the directory");
-
-    if (err) {
-        console.log("Error trying to get status of the directory" + err + "/// " + stat);
-        fs.mkdir("fsdata", function (error) {
-            if (error) {
-                console.log("Couldn't create directory" + err);
-            
-            } else {
-                writeFile();
-                console.log("Created the directory.");
-            }
-        });
-
-    } else {
-        console.log("The directory exists already.")
-        writeFile();
-
-    }
-});
-
-console.log("Wow, it's finally done!");
-
-var writeFile = function writeFile() {
+fs.readFile(fileName, "utf8", function(err, data){
+    if(err){
+        console.log("Error reading file \n" + err.message);
     
-    fs.writeFile(fileName, "Hello, there!", function(err){
-        if(err){
-            console.log("Nope." + err.message);
-
-        } else {
-            console.log ("Yay, it worked!");
-        }
-    });
-};
+    } else {
+        console.log(data);
+    }
+})
